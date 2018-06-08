28e3e499e848e6a7c4a2ee458ef6b93b74b161cc
Vitaly Kovalyshyn <v.kovalyshyn@webitel.com>
2018-06-08 14:45:13 +0300

    updated installer

--- install.sh
+++ install.sh
@@ -8,20 +8,21 @@ rm ~/.gitconfig
 rm ~/.gitignore
 rm ~/.tmux.conf
 
-echo "Symlinking files"
-ln -s ~/GitHub/dotfiles/vim ~/.vim
-ln -s ~/GitHub/dotfiles/vimrc ~/.vimrc
-ln -s ~/GitHub/dotfiles/zshrc ~/.zshrc
-ln -s ~/GitHub/dotfiles/tmux ~/.tmux.conf
-ln -s ~/GitHub/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/GitHub/dotfiles/gitignore ~/.gitignore
-ln -s ~/GitHub/dotfiles/scripts ~/.scripts
-ln -s ~/GitHub/dotfiles/config/i3 ~/.config/i3
-ln -s ~/GitHub/dotfiles/i3blocks.conf ~/.i3blocks.conf
-ln -s ~/GitHub/dotfiles/config/mpd ~/.config/mpd
-ln -s ~/GitHub/dotfiles/ncmpcpp ~/.ncmpcpp
-ln -s ~/GitHub/dotfiles/config/compton.conf ~/.config/compton.conf
-ln -s ~/GitHub/dotfiles/Xdefaults ~/.Xdefaults
-ln -s ~/GitHub/dotfiles/xinitrc ~/.xinitrc
+echo "Copy config files"
+cp -rf vim ~/.vim
+cp -rf vimrc ~/.vimrc
+cp -rf zshrc ~/.zshrc
+cp -rf tmux ~/.tmux.conf
+cp -rf gitconfig ~/.gitconfig
+cp -rf gitignore ~/.gitignore
+cp -rf scripts ~/.scripts
+cp -rf config/i3 ~/.config/i3
+cp -rf i3blocks.conf ~/.i3blocks.conf
+cp -rf config/mpd ~/.config/mpd
+cp -rf ncmpcpp ~/.ncmpcpp
+cp -rf config/compton.conf ~/.config/compton.conf
+cp -rf config/dunst ~/.config/dunst
+cp -rf Xdefaults ~/.Xdefaults
+cp -rf xinitrc ~/.xinitrc
 
 echo "All done."
